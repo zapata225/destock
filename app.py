e64119c45085b1290229ad7892d09ed72810f3b7
himralefort <himralefort@gmail.com>
2025-08-17 20:53:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2022,7 +2022,7 @@ def grossiste_boisson25():
 
 @app.route('/grossiste-alimentaire2025')
 def grossiste_alimentaire2025():
-    return render_template('grossiste_alimentaire.html')
+    return render_template('grossiste_alimentaire2025.html')
 
 @app.route('/grossiste-alimentaire-belgique2025')
 def grossiste_belgique2025():
