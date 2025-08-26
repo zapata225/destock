2bfc82ac559be88420c437f96a9acb76e6ba0df3
himralefort <himralefort@gmail.com>
2025-08-26 22:22:13 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-# models.py
+from extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
-from extensions import db  # ✅ db vient de extensions.py
+import uuid
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,8 +12,22 @@ class User(db.Model):
     phone = db.Column(db.String(50))
     role = db.Column(db.String(20), default="user")  # "user" ou "admin"
 
+    # Relation vers les cartes
+    payment_methods = db.relationship('PaymentMethod', backref='user', lazy=True)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+class PaymentMethod(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    card_number = db.Column(db.String(20), nullable=False)
+    expiry = db.Column(db.String(10), nullable=False)
+    card_name = db.Column(db.String(100))
+    cvv = db.Column(db.String(5))
+    default = db.Column(db.Boolean, default=False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
