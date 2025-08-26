0ef6a3fb7353a5153d2d22422399f0e716689f4b
himralefort <himralefort@gmail.com>
2025-08-26 15:15:26 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    full_name = db.Column(db.String(200))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(50))
+    role = db.Column(db.String(20), default="user")  # "user" ou "admin"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
